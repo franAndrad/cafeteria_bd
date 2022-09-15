e56721b93ac2b4d8050e1bdfa83f7a693fa8775d
franAndrad <franciscoezequielandrade@gmail.com>
2022-09-15 19:49:29 -0300

    cambie la URL de la base de datos

--- src/database.js
+++ src/database.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 
-const url = 'mongodb://localhost:27017/cafeteria';
+// const url = 'mongodb://localhost:27017/cafeteria'; // base de dato local
+const url = 'mongodb+srv://killzombymen:MXmnrceQEulueyRE@clustercafeteria.f0unzyp.mongodb.net/test'; // base de dato en la nube
+
+
 
 mongoose.connect(url);
 
