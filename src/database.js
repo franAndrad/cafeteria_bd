56d9a2a80e7d0cb66460519afc47ff205e3985ff
franAndrad <franciscoezequielandrade@gmail.com>
2022-09-15 20:04:04 -0300

    comente una segunda version para conectar la base de datos

--- src/database.js
+++ src/database.js
@@ -13,3 +13,21 @@ const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('base de datos conectada');
 })
+
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(url);
+//         console.log('BD conectada')
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// connectDB();
+
+// const connection = mongoose.connection;
+
+// // cuando se establecio la coneccion quiero ejecutar
+// connection.once('open', () => {
+//     console.log('base de datos conectada');
+// })
